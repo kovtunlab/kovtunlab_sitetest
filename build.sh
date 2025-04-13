f67ba1320a84b70625f144ad2dc67e903c396af1
KovtunLab <oleksiy.kovtun@icloud.com>
2025-04-13 14:28:53 +0200

    Add build script with Hugo installation

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+HUGO_VERSION="0.145.0"
 echo "Installing Hugo version ${HUGO_VERSION}"
 curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar -xz
 ./hugo --gc --minify
